fd0dda60bc03ba6d9f430dbeb1a49fb75617dd86
Timur Vorontsov <voroncov.timur0@gmail.com>
2024-04-26 15:46:08 +0600

    Изменил для исключения ошибки при вводе буквы в момент ввода суммы для пополнения

--- src/model/BillAcceptor.java
+++ src/model/BillAcceptor.java
@@ -6,11 +6,17 @@ public class BillAcceptor implements PaymentMethod {
     @Override
     public int addFunds(Scanner scanner) {
         System.out.print("Введите сумму купюры (50 или 100): ");
-        int amount = scanner.nextInt();
-        if (amount != 50 && amount != 100) {
-            System.out.println("Неверная сумма купюры. Пожалуйста, введите 50 или 100.");
+        if (scanner.hasNextInt()) {
+            int amount = scanner.nextInt();
+            if (amount != 50 && amount != 100) {
+                System.out.println("Неверная сумма купюры. Пожалуйста, введите 50 или 100.");
+                return 0;
+            }
+            return amount;
+        } else {
+            System.out.println("Неверный ввод. Пожалуйста, введите число.");
+            scanner.next();
             return 0;
         }
-        return amount;
     }
 }
--- src/model/CoinAcceptor.java
+++ src/model/CoinAcceptor.java
@@ -6,11 +6,17 @@ public class CoinAcceptor implements PaymentMethod {
     @Override
     public int addFunds(Scanner scanner) {
         System.out.print("Введите сумму монет (кратную 10): ");
-        int amount = scanner.nextInt();
-        if (amount % 10 != 0) {
-            System.out.println("Неверная сумма монет. Пожалуйста, введите сумму, кратную 10.");
+        if (scanner.hasNextInt()) {
+            int amount = scanner.nextInt();
+            if (amount % 10 != 0) {
+                System.out.println("Неверная сумма монет. Пожалуйста, введите сумму, кратную 10.");
+                return 0;
+            }
+            return amount;
+        } else {
+            System.out.println("Неверный ввод. Пожалуйста, введите число.");
+            scanner.next();
             return 0;
         }
-        return amount;
     }
 }
